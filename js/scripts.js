7bbfad83fd41dad8f75d8a4c25485918382d73d2
Noah Kittleson and Niem Nguyen <niem@>
2016-04-11 11:17:47 -0700

    added general card class and isSet function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+var colors = ["red", "purple", "green"];
+
+
+function Card (color) {
+  this.color = color;
+}
+
+function isSet(cardArray) {
+  //return true or false if it is in a set
+}
+
 $(document).ready(function() {
 
 });
