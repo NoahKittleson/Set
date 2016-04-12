53c64353d2dcf5d73f867f42b57d57d88289776f
NoahKittleson <noahkittleson@gmail.com>
2016-04-12 09:37:40 -0700

    added Deck creation and shuffling functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,38 @@ function Card (propertyArray) {
   this.properties = propertyArray;
 }
 
+Card.prototype.getID = function() {
+  var string = "";
+  this.properties.forEach(function(property) {
+    string = string.concat(property + " ");
+  });
+  return string;
+}
+
+function shuffle(array) {
+  var oldSpot, newSpot;
+  for (var i = 0; i < array.length; i++) {
+    var rand = Math.floor(Math.random() * i);
+    oldSpot = array[i-1];
+    array[i]
+  }
+}
+
+function createDeck() {
+  var deck = [];
+  properties[0].forEach(function(color) {
+    properties[1].forEach(function(shape) {
+      properties[2].forEach(function(number) {
+        properties[3].forEach(function(fill) {
+          deck.push(new Card([color, shape, number, fill]));
+        })
+      })
+    })
+  })
+  return deck;
+}
+
+
 function isSet(cardArray) {
   //return true or false if it is in a set
   var bool = true;
@@ -32,12 +64,14 @@ $(document).ready(function() {
   for (var i = 0; i < 12; i++) {
     cards.push(getRandomCard());
   }
-  
+  console.log(createDeck());
+  //console.log(cards[0].getID(););
+
   goodSet =[];
   goodSet.push(new Card(["red","squigly", 1, "emptty"]));
   goodSet.push(new Card(["green","sqruigly", 2, "efmpty"]));
   goodSet.push(new Card(["hi","squiggly", 3, "emmpty"]));
 
-  console.log(isSet(goodSet));
-  console.log(cards);
+  //console.log(isSet(goodSet));
+  //console.log(cards);
 });
