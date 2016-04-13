b2389075e2fd06d31d3bd6fee93de97a0334d221
Noah Kittleson <noahkittleson@gmail.com>
2016-04-13 11:38:04 -0700

    fixed minor bugs with card display, added score, and alert when deck runs out

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 var properties = [["red", "purple", "green"], ["squiggle", "diamond", "pill"], [1, 2, 3], ["empty", "crosshatch", "full"]];
 var cards = [];
+var score = 0;
 
 function Card (propertyArray) {
   this.properties = propertyArray;
 }
 
 function Deck () {
-  this.cards = createDeck().concat(createDeck());
+  this.cards = createDeck();
   this.shuffle();
 }
 
@@ -44,10 +45,6 @@ Deck.prototype.printDeck = function() {
 
 Deck.prototype.getNextCard = function() {
   return this.cards.pop();
-  // var rand = Math.floor(Math.random() * 1000);
-  // rand = rand % this.cards.length;
-  // var randomCard = this.cards.splice(rand,1);
-  // return randomCard[0];
 }
 
 Card.prototype.getID = function() {
@@ -84,17 +81,6 @@ function isDoubleSet(cardArray) {
   }
 }
 
-// function generateRandomCard() {
-//   var cardProperties = [];
-//   for (var i = 0; i < properties.length; i++) {
-//     var rand = Math.floor(Math.random() * 100);
-//     rand = rand % 3;
-//     cardProperties.push(properties[i][rand]);
-//   }
-//   var newCard = new Card (cardProperties);
-//   return newCard;
-// }
-
 $(document).ready(function() {
   var newDeck = new Deck();
   var cardIDArray = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13", "card14", "card15", "card16", "card17", "card18", "card19", "card20"];
@@ -119,20 +105,30 @@ $(document).ready(function() {
       var words = $(this).val().split(" ");
       parsedCards.push(new Card([words[0],words[1],words[2],words[3]]));
       chosenIDs.push($(this).attr("class"));
+      $(this).removeAttr('checked');
     });
     console.log(chosenIDs);
     console.log(parsedCards);
 
     //change cards if it is a set
     result = isSet(parsedCards);
-    alert(result);
     if (result) {
+      score++;
+      $("#score").text(score);
       for (var i = 0; i < chosenIDs.length; i++) {
-        newCard = newDeck.getNextCard();
-        console.log(newCard.getID());
-        $("." + chosenIDs[i]).attr("value", newCard.getID());
-        $("." + chosenIDs[i]).text(newCard.getID());
+        if (newDeck.cards.length) {
+          newCard = newDeck.getNextCard();
+          console.log(newCard.getID());
+          $("." + chosenIDs[i]).attr("value", newCard.getID());
+          str = newCard.getID();
+          str = str.substring(0, str.length-1);
+          $("." + chosenIDs[i]).attr("src", "img/" + str + ".png");
+        } else {
+          alert("out of cards");
+        }
       }
+    } else {
+      alert("Sorry, not a set");
     }
   });
 });
